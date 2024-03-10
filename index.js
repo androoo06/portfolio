08afa7c1b10b796ce1bcc7ec329519be3cbf46bd
Andr3wM1ll3r <an873997@ucf.edu>
2024-03-10 00:38:48 -0500

    update section display on user-scroll

--- index.js
+++ index.js
@@ -2,12 +2,13 @@ let openTab = "ABOUT"
 
 function setSelected(event) {
     let id = event.target.id.split("-")[1]
-
     let title = document.getElementById(`section-title-${id}`)
     let barrier = document.getElementById(`section-barrier-${id}`)
 
-    title.classList.remove("closed-section-title")
-    barrier.classList.remove("closed-section-barrier")
+    try{
+        title.classList.remove("closed-section-title")
+        barrier.classList.remove("closed-section-barrier")
+    } catch (e) {}
     
     title.classList.add("open-section-title")
     barrier.classList.add("open-section-barrier")
@@ -19,9 +20,11 @@ function removeSelected(event) {
     let barrier = document.getElementById(`section-barrier-${id}`)
 
     if (id !== openTab) {
-        title.classList.remove("open-section-title")
-        barrier.classList.remove("open-section-barrier")
-        
+        try{
+            title.classList.remove("open-section-title")
+            barrier.classList.remove("open-section-barrier")    
+        } catch (e) {}
+       
         title.classList.add("closed-section-title")
         barrier.classList.add("closed-section-barrier")
     }
@@ -34,21 +37,40 @@ $(function () {
     //     }, 2000);
     // });
 
+    $(window).on('scroll', function () {
+        let about = $('#about').offset().top - window.innerHeight
+        let projects = $('#projects').offset().top + ($('#projects').outerHeight()) - window.innerHeight
+        let experience = $('#experience').offset().top - window.innerHeight
+
+        function set(tab) {
+            setSelected({"target": {"id": `$-${tab}`}})
+
+            let temp = openTab
+            openTab = tab
+
+            removeSelected({"target": {"id": `$-${temp}`}})
+        }
+
+        if ($(window).scrollTop() >= experience) {
+            set("EXPERIENCE")
+        } else if ($(window).scrollTop() >= projects) {
+            set("PROJECTS")
+        } else if ($(window).scrollTop() >= about) {
+            set("ABOUT")
+        }
+    })
+
     document.querySelectorAll(".section-title-parent").forEach((parent) => {
         parent.onmouseenter = setSelected
         parent.onmouseleave = removeSelected
 
         parent.onclick = function(event) {
-            setSelected({
-                "target": {"id": `$-${event.target.id.split("-")[2]}`}
-            })
+            setSelected({"target": {"id": `$-${event.target.id.split("-")[2]}`}})
 
             let temp = openTab
             openTab = event.target.id.split("-")[2]
 
-            removeSelected({
-                "target": {"id": `$-${temp}`}
-            })
+            removeSelected({"target": {"id": `$-${temp}`}})
         }
     })
 })
